7d9551c1a0b7adebe6594a1f397c157aa709fafd
Eldee05 <eldivinesolace@gmail.com>
2026-04-01 15:07:44 +0100

    removed-createContextimport- moved-useSidebar-here,

--- src/hooks/useSidebar.ts
+++ src/hooks/useSidebar.ts
@@ -1,14 +1,7 @@
 import * as React from "react";
-import { createContext } from "react";
+import { SidebarContext } from "@/components/ui/sidebar";
 
-interface SidebarContextValue {
-  isOpen: boolean;
-  toggle: () => void;
-}
-
-const SidebarContext = createContext<SidebarContextValue | null>(null);
-
-export function useSidebar() {
+export default function useSidebar() {
   const context = React.useContext(SidebarContext);
   if (!context) {
     throw new Error("useSidebar must be used within a SidebarProvider.");
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Toaster as Sonner, toast } from "sonner";
+
+export { toast };
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
